1737e5f798da78efb337f31523eddc6ed82426dd
Unknown <goberna_ana@hotmail.com>
2017-09-13 13:48:07 -0300

    primera fila de enemigos

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,11 +3,13 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	private var wachin:Wachin;
+	private var malitos:FlxTypedGroup<Malitos>;
 	
 	override public function create():Void
 	{
@@ -15,7 +17,14 @@ class PlayState extends FlxState
 		
 		FlxG.cameras.bgColor = 0xff96ceb4;
 		wachin = new Wachin(FlxG.width / 2, FlxG.height, AssetPaths.wachin__png);
+		malitos = new FlxTypedGroup<Malitos>();
 		
+		for (i in 0...5)
+		{
+			var malito1:Malitos = new Malitos(10 + 30*i,50,AssetPaths.malo__png);
+			malitos.add(malito1);
+		}
+		add(malitos);
 		add(wachin);
 	}
 
